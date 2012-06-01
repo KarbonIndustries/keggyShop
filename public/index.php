1732bcbd74450b150f849687ec1f5da37a8038ba
Shammel Lee <shammel@karbonnyc.com>
2012-06-01 19:27:38 -0400

    2nd commit

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+	<title>Keggy Shop</title>
+	<link rel="stylesheet" href="/css/styles.css" type="text/css" media="all" charset="utf-8">
+</head>
+<body>
+	
+</body>
+</html>
